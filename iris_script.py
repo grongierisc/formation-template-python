baacdfce1834d9308d91e4c5468271455e1e9b9d
grongierisc <guillaume.rongier@intersystems.com>
2026-05-26 10:06:58 +0200

    fix: Comment out demo data loading section in iris_script.py

--- iris_script.py
+++ iris_script.py
@@ -15,15 +15,15 @@ iris.cls('Security.Users').UnExpireUserPasswords("*")
 iris.system.Process.SetNamespace("IRISAPP")
 
 # load demo data
-engine = create_engine('iris+emb:///')
-# list all csv files in the demo data folder
-for files in glob.glob('/irisdev/app/misc/*.csv'):
-    # get the file name without the extension
-    table_name = 'training'
-    # load the csv file into a pandas dataframe
-    df = pd.read_csv(files)
-    # write the dataframe to IRIS
-    df.to_sql(table_name, engine, if_exists='replace', index=False, schema='iris')
+# engine = create_engine('iris+emb:///')
+# # list all csv files in the demo data folder
+# for files in glob.glob('/irisdev/app/misc/*.csv'):
+#     # get the file name without the extension
+#     table_name = 'training'
+#     # load the csv file into a pandas dataframe
+#     df = pd.read_csv(files)
+#     # write the dataframe to IRIS
+#     df.to_sql(table_name, engine, if_exists='replace', index=False, schema='iris')
 
 # load interop demo
 try:
